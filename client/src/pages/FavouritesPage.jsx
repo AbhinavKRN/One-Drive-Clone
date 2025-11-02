33183cd3804156d9a3540e98368a07f2300f2594
AbhinavKRNarayan <115357208+AbhinavKRN@users.noreply.github.com>
2025-11-03 03:39:12 +0530

    updated photos

--- client/src/pages/FavouritesPage.jsx
+++ client/src/pages/FavouritesPage.jsx
@@ -7,7 +7,6 @@ const FavouritesPage = () => {
     <div className="favs-page">
       <h1 className="favs-header">Favourites</h1>
       <div className="favs-hero">
-
         <div className="favs-stack">
           <img src="/images/image_copy2.png" alt="Memories" />
         </div>
@@ -15,15 +14,15 @@ const FavouritesPage = () => {
           <h2 className="favs-no-text">No favourite Yet</h2>
           <h4>mark photos and videos as favourite to see them here</h4>
         </div>
-      </div>
 
-      <div className="favs-search-container">
-        <button className="favs-add-btn">
-          <AddRegular />
-        </button>
-        <div className="favs-search-bar">
-          <SearchRegular />
-          <input type="text" placeholder="Search your photos" />
+        <div className="favs-search-container">
+          <button className="favs-add-btn">
+            <AddRegular />
+          </button>
+          <div className="favs-search-bar">
+            <SearchRegular />
+            <input type="text" placeholder="Search your photos" />
+          </div>
         </div>
       </div>
     </div>
